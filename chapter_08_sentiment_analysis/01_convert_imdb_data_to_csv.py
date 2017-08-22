c18c4430806b06f859b602f48409705b5d9c9d78
jean28 <jean.mendez2@upr.edu>
2017-08-21 22:07:22 -0400

    Convert IMDB data to CSV.

---
+++ chapter_08_sentiment_analysis/01_convert_imdb_data_to_csv.py
@@ -0,0 +1,32 @@
+import os
+import numpy as np
+import pandas as pd
+
+create_csv_file = False
+
+if create_csv_file:
+    labels = {'pos': 1, 'neg':0}
+
+    df = pd.DataFrame()
+
+    print('starting:')
+    for s in ('test', 'train'):
+        for l in ('pos', 'neg'):
+            path = '../data/aclImdb/%s/%s' % (s, l)
+            for file in os.listdir(path):
+                with open(os.path.join(path, file), 'r') as infile:
+                    txt = infile.read()
+                print('lol file')
+                df = df.append([[txt, labels[l]]], ignore_index=True)
+
+    print('done')
+    df.columns = ['review', 'sentiment']
+
+    np.random.seed(0)
+
+    df = df.reindex(np.random.permutation(df.index))
+    df.to_csv('../data/movie_data.csv')
+    df.head(3)
+
+df = pd.read_csv('../data/movie_data.csv')
+print(df.head(3))
