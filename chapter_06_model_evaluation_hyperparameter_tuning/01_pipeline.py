664dead980bcda0dc25f01c57b9fc2d09514c26d
jean28 <jean.mendez2@upr.edu>
2017-08-29 23:34:08 -0400

    Pipeline example

---
+++ chapter_06_model_evaluation_hyperparameter_tuning/01_pipeline.py
@@ -0,0 +1,29 @@
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+from sklearn.linear_model import LogisticRegression
+from sklearn.pipeline import Pipeline
+
+df = pd.read_csv('../data/wdbc.csv')
+
+# Convert M (malign) and B (benign) into numbers
+
+X = df.iloc[:, 2:].values
+y = df.iloc[:, 1].values
+
+le = LabelEncoder()
+y = le.fit_transform(y)
+
+le.transform(['M', 'B'])
+
+# Divide Dataset into separate training (80%) and testing (20%)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=1)
+
+pipe_lr = Pipeline([('scl', StandardScaler()),
+                    ('pca', PCA(n_components=2)),
+                    ('clf', LogisticRegression(random_state=1))])
+
+pipe_lr.fit(X_train, y_train)
+print('Test Accuracy: %.3f' % pipe_lr.score(X_test, y_test))
