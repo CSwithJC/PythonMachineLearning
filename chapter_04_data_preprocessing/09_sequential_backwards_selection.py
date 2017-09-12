1ac23796c8b48f49435bd31d59540b32ff14c0bc
jean28 <jean.mendez2@upr.edu>
2017-09-11 21:03:07 -0400

    testing of the SBS algorithm

--- chapter_04_data_preprocessing/09_sequential_backwards_selection.py
+++ chapter_04_data_preprocessing/09_sequential_backwards_selection.py
@@ -1,8 +1,14 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
 from sklearn.base import clone
 from itertools import combinations
-import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.preprocessing import StandardScaler
+
 
 
 class SBS():
@@ -71,3 +77,55 @@ class SBS():
         y_pred = self.estimator.predict(X_test[:, indices])
         score = self.scoring(y_test, y_pred)
         return score
+
+df_wine = pd.read_csv('../data/wine.csv')
+df_wine.columns = ('Class label', 'Alcohol',
+                   'Malic acid', 'Ash',
+                   'Alcalinity of ash',
+                   'Magnesium',
+                   'Total phenola',
+                   'Flavanoids',
+                   'Nonflavanoid phenols',
+                   'Proanthocyanins',
+                   'Color intensity', 'Hue',
+                   'OD280/OD315 of diluted wines',
+                   'Proline')
+
+
+X, y = df_wine.iloc[:, 1:].values, df_wine.iloc[:, 0].values
+X_train, X_test, y_train, y_test = train_test_split(X, y,
+                                                    test_size=0.3,
+                                                    random_state=0)
+
+stdsc = StandardScaler()
+X_train_std = stdsc.fit_transform(X_train)
+X_test_std = stdsc.fit_transform(X_test)
+
+
+knn = KNeighborsClassifier(n_neighbors=2)
+sbs = SBS(knn, k_features=1)
+sbs.fit(X_train_std, y_train)
+
+# Plot the classification accuracy of the KNN classifier that was calculated
+# on the validation dataset.
+
+k_feat = [len(k) for k in sbs.subsets_]
+plt.plot(k_feat, sbs.scores_, marker='o')
+plt.ylim([0.7, 1.1])
+plt.ylabel('Accuracy')
+plt.xlabel('Number of features')
+plt.grid()
+plt.show()
+
+k5 = list(sbs.subsets_[8])
+print(df_wine.columns[1:][k5])
+
+# Using original data set:
+knn.fit(X_train_std, y_train)
+print('Training accuracy: ', knn.score(X_train_std, y_train))
+print('Test accuracy: ', knn.score(X_test_std, y_test))
+
+# Using selected 5-feature subset:
+knn.fit(X_train_std[:, k5], y_train)
+print('Training accuracy: ', knn.score(X_train_std[:, k5], y_train))
+print('Test accuracy: ', knn.score(X_test_std[:, k5], y_test))
