4b146e46657e7731fba2693ce4ee067a6de00f07
jean28 <jean.mendez2@upr.edu>
2017-12-16 22:26:06 -0500

    add givens to code

--- chapter_13_parallel_nn_training_theano/03_shared_memory.py
+++ chapter_13_parallel_nn_training_theano/03_shared_memory.py
@@ -3,10 +3,12 @@ import numpy as np
 from theano import tensor as T
 
 # Config Theano to use 32-bit architecture:
-#theano.config.floatX = 'float32'
+theano.config.floatX = 'float32'
 #theano.config.device = 'gpu'
 
 # initialize
+data = np.array([[1, 2, 3]],
+                dtype=theano.config.floatX)
 x = T.fmatrix(name='x')
 w = theano.shared(np.asarray([[0.0, 0.0, 0.0]],
                              dtype=theano.config.floatX))
@@ -15,7 +17,9 @@ update = [[w, w+1.0]]
 
 # compile
 net_imput = theano.function(inputs=[x],
-                            updates=update,#  Update variable
+                            updates=update, # Update variable
+                            givens={x: data}, # Givens is used to insert values into a graph
+                                              # before compiling it
                             outputs=z)
 
 # execute
