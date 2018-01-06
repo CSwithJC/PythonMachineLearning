91faa2a1797b9cdddb964bc5d879949b70c3dc6d
jean28 <jean.mendez2@upr.edu>
2018-01-06 01:18:27 -0500

    k-means++

---
+++ chapter_11_clustering_analysis/02_k_mean++.py
@@ -0,0 +1,34 @@
+import matplotlib.pyplot as plt
+
+from sklearn.datasets import make_blobs
+from sklearn.cluster import KMeans
+
+"""Plot blobs that will be used for K-Means later"""
+X, y = make_blobs(n_samples=150,
+                  n_features=2,
+                  centers=3,
+                  cluster_std=0.5,
+                  shuffle=True,
+                  random_state=0)
+
+plt.scatter(X[:, 0],
+            X[:, 1],
+            c='red',
+            marker='o',
+            s=50)
+
+plt.grid()
+plt.show()
+
+""" Run K-Means++ on the data: """
+km = KMeans(n_clusters=3,
+            init='k-means++',  # K-Means++!!
+            n_init=10,
+            max_iter=300,
+            tol=1e-04,
+            random_state=0)
+
+y_km = km.fit_predict(X)
+
+print('K-Means result:')
+print(y_km)
